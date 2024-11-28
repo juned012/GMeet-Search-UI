ab0ed65a064fb975e56279e552bca3abb7766f8f
juned072 <junedansari9172@gmail.com>
2024-11-28 21:19:29 +0530

    Implemented random color to all profile background

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const userData = [
-  { nameFirstLetter: "A", fullName: "Aniket" },
   { nameFirstLetter: "B", fullName: "Bhavana" },
   { nameFirstLetter: "C", fullName: "Chirag" },
   { nameFirstLetter: "D", fullName: "Dipali" },
@@ -13,14 +12,34 @@ const userData = [
 
 const usersContainer = document.getElementById("usersContainer");
 
+const colorNames = [
+  "Red",
+  "Green",
+  "Blue",
+  "Yellow",
+  "Orange",
+  "Purple",
+  "Pink",
+  "Black",
+  "Gray",
+  "Cyan",
+  "Magenta",
+  "Brown",
+  "Teal",
+  "Gold",
+];
+
 userData.forEach((user) => {
+  const randomIndex = Math.floor(Math.random() * colorNames.length);
+  const randomColor = colorNames[randomIndex];
+
   usersContainer.innerHTML += `
-      <div class="user">
-        <div class="userIcon">
-          <p>${user.nameFirstLetter}</p>
-        </div>
-        <div class="userName">
-          <h3>${user.fullName}</h3>
-        </div>
-      </div>`;
+    <div class="user">
+      <div class="userIcon" style="background-color: ${randomColor};">
+        <p>${user.nameFirstLetter}</p>
+      </div>
+      <div class="userName">
+        <h3>${user.fullName}</h3>
+      </div>
+    </div>`;
 });
