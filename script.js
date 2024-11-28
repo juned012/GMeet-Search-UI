0cb3a6948a5dd2915a1680127cee4c6d384e6f7d
juned072 <junedansari9172@gmail.com>
2024-11-28 21:41:03 +0530

    Added a search functionality

--- script.js
+++ script.js
@@ -29,17 +29,37 @@ const colorNames = [
   "Gold",
 ];
 
-userData.forEach((user) => {
-  const randomIndex = Math.floor(Math.random() * colorNames.length);
-  const randomColor = colorNames[randomIndex];
+function showUser(data) {
+  usersContainer.innerHTML = "";
+  if (data.length === 0) {
+    usersContainer.innerHTML = `<p>No users found.</p>`;
+    return;
+  }
 
-  usersContainer.innerHTML += `
-    <div class="user">
-      <div class="userIcon" style="background-color: ${randomColor};">
-        <p>${user.nameFirstLetter}</p>
-      </div>
-      <div class="userName">
-        <h3>${user.fullName}</h3>
-      </div>
-    </div>`;
-});
+  data.forEach((user) => {
+    const randomIndex = Math.floor(Math.random() * colorNames.length);
+    const randomColor = colorNames[randomIndex];
+
+    usersContainer.innerHTML += `
+      <div class="user">
+        <div class="userIcon" style="background-color: ${randomColor};">
+          <p>${user.nameFirstLetter}</p>
+        </div>
+        <div class="userName">
+          <h3>${user.fullName}</h3>
+        </div>
+      </div>`;
+  });
+}
+
+function searchBar() {
+  const inputField = document.getElementById("inputField");
+  const inputVal = inputField.value.toLowerCase();
+
+  const filteredData = userData.filter((user) =>
+    user.fullName.toLowerCase().includes(inputVal)
+  );
+
+  showUser(filteredData);
+}
+showUser(userData);
